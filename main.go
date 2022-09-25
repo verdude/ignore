866fd57e7612da320ae94fbc72a93251970daea1
Santiago Verdu <santi@getlovage.com>
2022-09-24 19:18:03 -0600

    stars?

--- main.go
+++ main.go
@@ -1,88 +1,96 @@
 package main
 
 import (
-	"golang.org/x/exp/slices"
-	"log"
-	"os"
+  "golang.org/x/exp/slices"
+  "log"
+  "os"
   "io"
   "io/ioutil"
-	"strings"
+  "strings"
+  "flag"
 )
 
 func parse(contents string) []string {
-	entries := make([]string, 0)
-	lines := strings.Split(strings.TrimSpace(contents), "\n")
-	for _, line := range lines {
-		str := strings.TrimSpace(line)
-		if len(str) > 0 {
-			entries = append(entries, str)
-		}
-	}
-	return entries
+  entries := make([]string, 0)
+  lines := strings.Split(strings.TrimSpace(contents), "\n")
+  for _, line := range lines {
+    str := strings.TrimSpace(line)
+    if len(str) > 0 {
+      entries = append(entries, str)
+    }
+  }
+  return entries
 }
 
 func getLines(fname string, create bool) []string {
-	f, err := os.Open(fname)
-	if err != nil {
-		if create {
-			f, err = os.Create(fname)
-			if err != nil {
-				log.Println("Could not create", fname)
-				log.Fatal("ERROR: ", err)
-			} else {
-				log.Println("Created", fname)
-				f.Close()
-				return make([]string, 0)
-			}
-		}
-		log.Fatal(fname, " not found.")
-	}
-	defer f.Close()
+  f, err := os.Open(fname)
+  if err != nil {
+    if create {
+      f, err = os.Create(fname)
+      if err != nil {
+        log.Println("Could not create", fname)
+        log.Fatal("ERROR: ", err)
+      } else {
+        log.Println("Created", fname)
+        f.Close()
+        return make([]string, 0)
+      }
+    }
+    log.Fatal(fname, " not found.")
+  }
+  defer f.Close()
 
-	b, err := ioutil.ReadFile(fname)
-	if err != nil {
+  b, err := ioutil.ReadFile(fname)
+  if err != nil {
     if err != io.EOF {
       log.Fatal("read fail: ", err)
     }
-	}
-	return parse(string(b))
+  }
+  return parse(string(b))
 }
 
 func isMissing(patterns []string, str string) bool {
-	_, found := slices.BinarySearch(patterns, str)
-	return !found
+  _, found := slices.BinarySearch(patterns, str)
+  return !found
 }
 
-func writeIgnore(patterns []string) {
-	f, err := os.OpenFile(".gitignore", os.O_WRONLY, 0444)
-	if err != nil {
-		log.Fatal("Failed to write to file")
-	}
-	defer f.Close()
+func writeIgnore(patterns []string, stdout bool) {
+  var f *os.File
+  if stdout {
+    f = os.Stdout
+  } else {
+    f, err := os.OpenFile(".gitignore", os.O_WRONLY, 0444)
+    if err != nil {
+      log.Fatal("Failed to write to file")
+    }
+    defer f.Close()
+  }
 
-	for _, pattern := range patterns {
-		f.Write([]byte(pattern))
-		f.Write([]byte("\n"))
-	}
+  for _, pattern := range patterns {
+    f.Write([]byte(pattern))
+    f.Write([]byte("\n"))
+  }
 }
 
 func collectPatterns() []string {
-	presets := getLines("/etc/ignoregit/presets.txt", true)
-	patterns := getLines(".gitignore", true)
-	missing := make([]string, 0)
+  presets := getLines("/etc/ignoregit/presets.txt", true)
+  patterns := getLines(".gitignore", true)
+  missing := make([]string, 0)
 
-	slices.Sort(patterns)
+  slices.Sort(patterns)
 
-	for _, preset := range presets {
-		if isMissing(patterns, preset) {
-			missing = append(missing, preset)
-		}
-	}
-	patterns = append(patterns, missing...)
-	return patterns
+  for _, preset := range presets {
+    if isMissing(patterns, preset) {
+      missing = append(missing, preset)
+    }
+  }
+  patterns = append(patterns, missing...)
+  return patterns
 }
 
 func main() {
-	patterns := collectPatterns()
-	writeIgnore(patterns)
+  var stdout = flag.Bool("1", false, "Write to stdout instead of to a file")
+  flag.Parse()
+  patterns := collectPatterns()
+  writeIgnore(patterns, *stdout)
 }
